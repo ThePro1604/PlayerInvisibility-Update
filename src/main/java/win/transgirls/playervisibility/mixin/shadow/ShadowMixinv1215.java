b2e48c8d738bad331337b61d2a582b7969b0854c
ThePro1604 <shahaf.sto@gmail.com>
2026-04-01 13:21:03 +0300

    Fix Mixin configuration errors - remove broken fire/hitbox mixins and fix shadow mixin parameter type

--- src/main/java/win/transgirls/playervisibility/mixin/shadow/ShadowMixinv1215.java
+++ src/main/java/win/transgirls/playervisibility/mixin/shadow/ShadowMixinv1215.java
@@ -4,6 +4,7 @@ import win.transgirls.playervisibility.PlayerVisibility;
 import win.transgirls.playervisibility.config.ModConfig;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.state.EntityRenderState;
+import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ShadowMixinv1215 {
     // Hide shadow by clearing the shadow pieces list when rendering hidden entities
     @Inject(method = "updateShadow(Lnet/minecraft/entity/Entity;Lnet/minecraft/client/render/entity/state/EntityRenderState;)V", at = @At("HEAD"), cancellable = true)
-    private void injectShadow(Object entity, EntityRenderState renderState, CallbackInfo ci) {
+    private void injectShadow(Entity entity, EntityRenderState renderState, CallbackInfo ci) {
         if (ModConfig.hideShadows && PlayerVisibility.shouldHideEntityRenderState(renderState)) {
             // Clear shadow by preventing shadow update
             ci.cancel();
